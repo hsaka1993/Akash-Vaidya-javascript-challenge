51ca725b173a7e135e9a26f954986738b373470b
Akash Vaidya <akashvaidya17@gmail.com>
2020-08-23 19:01:21 -0700

    update

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var body = d3.select("tbody")
+var i;
+for (i = 0; i <tableData.length; i++) {
+var row = body.append("tr")
+row.append("td").text(tableData[i].datetime)
+row.append("td").text(tableData[i].city)
+row.append("td").text(tableData[i].state)
+row.append("td").text(tableData[i].country)
+row.append("td").text(tableData[i].shape)
+row.append("td").text(tableData[i].durationMinutes)
+row.append("td").text(tableData[i].comments)
+}
+function search(){
+
+    alert("hello")
+}
